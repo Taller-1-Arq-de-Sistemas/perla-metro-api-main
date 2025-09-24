46dc4d0d521a33f7b1444e28d053ce17c6cd263c
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2025-09-24 17:04:33 -0300

    Feat: add Station service class and station service interface

---
+++ src/Services/Interfaces/IStationService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace perla_metro_api_main.src.Services.Interfaces
+{
+    public interface IStationService
+    {
+        
+    }
+}
---
+++ src/Services/StationService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using perla_metro_api_main.src.Services.Interfaces;
+
+namespace perla_metro_api_main.src.Services
+{
+    public class StationService : IStationService
+    {
+
+
+
+    }
+}
